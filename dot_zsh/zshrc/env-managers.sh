3ec2556ff41ff238c5d2ab31de312a3ae3409e48
fushimi <rfushimi@users.noreply.github.com>
2025-02-11 21:36:19 +0900

    dotfiles sync

--- dot_zsh/zshrc/env-managers.sh
+++ dot_zsh/zshrc/env-managers.sh
@@ -1,4 +1,4 @@
 # activate global python environment
-if [[ -f ~/.local/share/uv/.venv/bin/activate ]]; then
-    source ~/.local/share/uv/.venv/bin/activate
-fi
+# if [[ -f ~/.local/share/uv/.venv/bin/activate ]]; then
+#     source ~/.local/share/uv/.venv/bin/activate
+# fi
