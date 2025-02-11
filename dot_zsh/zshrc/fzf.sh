271fe1808b3facb7bbbfd84e7e53bf473911ee7b
fushimi <rfushimi@users.noreply.github.com>
2025-02-12 01:37:03 +0900

    dotfiles sync

--- dot_zsh/zshrc/fzf.sh
+++ dot_zsh/zshrc/fzf.sh
@@ -1,7 +1,7 @@
 # c - browse chrome history
 c() {
   local cols sep google_history open
-  cols=$(( COLUMNS / 3 ))
+  cols=$((COLUMNS / 3))
   sep='{::}'
 
   if [ "$(uname)" = "Darwin" ]; then
@@ -15,41 +15,22 @@ c() {
   sqlite3 -separator $sep /tmp/h \
     "select substr(title, 1, $cols), url
      from urls order by last_visit_time desc" |
-  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-  fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open > /dev/null 2> /dev/null
+    awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+    fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open >/dev/null 2>/dev/null
 }
 
 man-find() {
-    f=$(fd . $MANPATH/man${1:-1} -t f -x echo {/.} | fzf) && man $f
+  f=$(fd . $MANPATH/man${1:-1} -t f -x echo {/.} | fzf) && man $f
 }
 
-# fzf history
+# if command -v fzf >/dev/null 2>&1; then
+#   source <(fzf --zsh)
+# fi
+
 function fzf-select-history() {
-    BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
-    CURSOR=$#BUFFER
-    zle reset-prompt
+  BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER")
+  CURSOR=$#BUFFER
+  zle reset-prompt
 }
 zle -N fzf-select-history
 bindkey '^r' fzf-select-history
-
-# cdr自体の設定
-if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]]; then
-    autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
-    add-zsh-hook chpwd chpwd_recent_dirs
-    zstyle ':completion:*' recent-dirs-insert both
-    zstyle ':chpwd:*' recent-dirs-default true
-    zstyle ':chpwd:*' recent-dirs-max 1000
-fi
-
-# fzf cdr
-function fzf-cdr() {
-    local selected_dir=$(cdr -l | awk '{ print $2 }' | fzf --reverse)
-    if [ -n "$selected_dir" ]; then
-        BUFFER="cd ${selected_dir}"
-        zle accept-line
-    fi
-    zle clear-screen
-}
-zle -N fzf-cdr
-setopt noflowcontrol
-bindkey '^q' fzf-cdr
