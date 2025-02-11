42297048861370b7ce1c88fe110ada250bf493fe
fushimi <rfushimi@users.noreply.github.com>
2025-02-11 18:53:11 +0900

    dotfiles sync

--- setup/common/run_akey.sh
+++ setup/common/run_akey.sh
@@ -5,11 +5,12 @@ if [[ ! -f $HOME/.ssh/akey ]]; then
         echo "Installing akey: bitwarden-cli not found"
         exit 1
     fi
-
     if ! command -v jq >/dev/null 2>&1; then
         echo "Installing akey: jq not found"
         exit 1
     fi
+    bw login fushimi.ryohei@gmail.com
+    export BW_SESSION=$(bw unlock --raw)
     bw get item akey | jq -r '.notes' > $HOME/.ssh/akey
     chmod 600 $HOME/.ssh/akey
 fi
