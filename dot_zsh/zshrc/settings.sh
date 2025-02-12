ff4978becdca158d3f2cadf7e1095d0c3a6f82d8
fushimi <rfushimi@users.noreply.github.com>
2025-02-12 21:20:13 +0900

    dotfiles sync

--- dot_zsh/zshrc/settings.sh
+++ dot_zsh/zshrc/settings.sh
@@ -3,6 +3,8 @@ setopt share_history
 export HISTFILE=~/.zsh_history
 export HISTSIZE=1000000
 export SAVEHIST=1000000
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_SAVE_NO_DUPS
 
 # コマンド実行後すぐに履歴ファイルへ追記
 setopt inc_append_history
