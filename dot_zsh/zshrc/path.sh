7230b44dd37f584d4cc3330a6dfab625ead947e1
fushimi <rfushimi@users.noreply.github.com>
2025-10-20 10:45:09 +0900

    dotfiles sync

--- dot_zsh/zshrc/path.sh
+++ dot_zsh/zshrc/path.sh
@@ -42,7 +42,10 @@ esac
 # bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
-[ -s "$BUN_INSTALL/_bun" ] && source "$BUN_INSTALL/_bun"
+# Load bun completions only if compinit is available
+if [ -s "$BUN_INSTALL/_bun" ] && (( $+functions[compdef] )); then
+    source "$BUN_INSTALL/_bun"
+fi
 
 # nvm
 export NVM_DIR="$HOME/.config/nvm"
