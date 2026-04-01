0d762390078c1890e4aa7c426c82d5dd85019b2f
fushimi <rfushimi@users.noreply.github.com>
2026-04-01 08:42:11 +0000

    dotfiles sync

--- dot_zsh/zshrc/alias.sh
+++ dot_zsh/zshrc/alias.sh
@@ -3,9 +3,6 @@
 # Source common aliases
 source ~/.common_alias.sh
 
-# save current directory and source ~/.zshrc and get back to the directory
-alias reload="pwd > ~/.zshrc_pwd; source ~/.zshrc; cd `cat ~/.zshrc_pwd`"
-
 alias ud="update-dotfiles; chezmoi apply; source ~/.zshrc"
 alias sz="source ~/.zshrc"
 alias zs="source ~/.zshrc"
