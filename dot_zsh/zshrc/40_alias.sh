2655667d1bab6b5cb151689bd86c6f5a97f10972
fushimi <fushimi@google.com>
2026-05-19 12:22:28 +0900

    dotfiles sync 2026-05-19

--- dot_zsh/zshrc/40_alias.sh
+++ dot_zsh/zshrc/40_alias.sh
@@ -3,8 +3,6 @@
 # Source common aliases
 [ -f "$HOME/.common_alias.sh" ] && source "$HOME/.common_alias.sh"
 
-alias reload="source ~/.zshrc"
-
 ud() {
     update-dotfiles "$@"
     chezmoi apply
@@ -12,17 +10,6 @@ ud() {
 }
 
 alias sz="source ~/.zshrc"
-alias zs="source ~/.zshrc"
-
-# ============================================================================
-# Git Aliases (that might be shell specific)
-# ============================================================================
-alias g="git"
-alias ga="git add"
-alias gs="git status"
-alias gc="git commit"
-alias gp="git push"
-alias gl="git pull"
 
 # Draftbox: quick note-taking in VS Code
 alias draft='code ~/Draftbox --profile "Draftbox"'
